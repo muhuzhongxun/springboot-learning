5a04ce4dfc2968403562749432efbf4cce98a434
muhuzhongxun <983724494@qq.com>
2021-07-10 16:09:40 +0800

    null

---
+++ springboot02config/src/main/java/com/xu/springboot02config/Springboot02configApplication.java
@@ -0,0 +1,13 @@
+package com.xu.springboot02config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02configApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02configApplication.class, args);
+    }
+
+}
