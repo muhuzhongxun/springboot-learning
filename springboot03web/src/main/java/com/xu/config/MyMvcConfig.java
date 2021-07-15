6f556ffd3fc4233aa2acc806e75d6c5e2033575c
muhuzhongxun <983724494@qq.com>
2021-07-15 21:00:42 +0800

    thymeleaf配置国际化页面

--- springboot03web/src/main/java/com/xu/config/MyMvcConfig.java
+++ springboot03web/src/main/java/com/xu/config/MyMvcConfig.java
@@ -1,10 +1,8 @@
 package com.xu.config;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Locale;
 
 //拓展speingmvc  dispatcherservlet
 @Configuration
