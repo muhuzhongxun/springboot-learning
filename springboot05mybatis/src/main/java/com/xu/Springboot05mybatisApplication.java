ba6284d8578ee7421962b22161ce4c13584f62cb
ouhuangzhongxun <983724494@qq.com>
2021-07-23 23:42:15 +0800

    .

--- springboot05mybatis/src/main/java/com/xu/Springboot05mybatisApplication.java
+++ springboot05mybatis/src/main/java/com/xu/Springboot05mybatisApplication.java
@@ -1,6 +1,5 @@
-package com.xu.springboot05mybatis;
+package com.xu;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
