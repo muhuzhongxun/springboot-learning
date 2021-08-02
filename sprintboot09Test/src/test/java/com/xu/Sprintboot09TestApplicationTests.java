fe4f1a72c137399a9c66b1cb3982bb31e98e1ac3
ouhuangzhongxun <983724494@qq.com>
2021-08-02 23:48:10 +0800

    封装了一方法

--- sprintboot09Test/src/test/java/com/xu/Sprintboot09TestApplicationTests.java
+++ sprintboot09Test/src/test/java/com/xu/Sprintboot09TestApplicationTests.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class Sprintboot09TestApplicationTests {
@@ -28,11 +31,46 @@ class Sprintboot09TestApplicationTests {
     }
 
     @Test
-    void contextLoads2() {
+    void contextLoads2() throws MessagingException {
         //发送复杂的邮件
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        //组装
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true);
 
-        MimeMessage mimeMessage = new MimeMessage();
+        mimeMessageHelper.setSubject("喵喵喵！");
+        mimeMessageHelper.setText("<p style='color: red'>这里是猪妙</p>",true);
+        //附件
+        mimeMessageHelper.addAttachment("01.jpg",new File("src/main/resources/static/jpg/01.jpg"));
 
+        mimeMessageHelper.setTo("2819991349@qq.com");
+        mimeMessageHelper.setFrom("983724494@qq.com");
+        mailSender.send(mimeMessage);
+    }
+
+    /**
+     * 封装个自己的方法
+     * @param html: 支持可选文本的多部分消息
+     * @param Subject: 邮件标题
+     * @param Text: 邮件正文内容
+     * @param htmlControl: 标签转义
+     * @param To: 收件人邮箱
+     * @param From: 发件人邮箱
+     * @throws MessagingException
+     * @Autor muhuzhongxun 2021/8/2 23:46:00
+     */
+    public void sendMail(boolean html,String Subject,String Text,boolean htmlControl,String To,String From) throws MessagingException {
+        //发送复杂的邮件
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        //组装
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,html);
+
+        mimeMessageHelper.setSubject(Subject);
+        mimeMessageHelper.setText(Text,htmlControl);
+        //附件
+        mimeMessageHelper.addAttachment("01.jpg",new File("src/main/resources/static/jpg/01.jpg"));
+
+        mimeMessageHelper.setTo(To);
+        mimeMessageHelper.setFrom(From);
         mailSender.send(mimeMessage);
     }
 
